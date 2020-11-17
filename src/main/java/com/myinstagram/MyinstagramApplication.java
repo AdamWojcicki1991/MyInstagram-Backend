8f0dd3b489554140f1012567a332f28c1ef67e0d
Adam Wojcicki <adamwojcicki1991@gmail.com>
2020-11-17 11:54:05 +0100

    first commit

---
+++ src/main/java/com/myinstagram/MyinstagramApplication.java
@@ -0,0 +1,13 @@
+package com.myinstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyinstagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyinstagramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myinstagram/MyinstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myinstagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyinstagramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
