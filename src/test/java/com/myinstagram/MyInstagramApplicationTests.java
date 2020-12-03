8f5758152f859913746bc2d9b373b103a12498ae
Adam Wojcicki <adamwojcicki1991@gmail.com>
2020-12-04 00:36:21 +0100

    Add @EnableAsync annotation on test context load

--- src/test/java/com/myinstagram/MyInstagramApplicationTests.java
+++ src/test/java/com/myinstagram/MyInstagramApplicationTests.java
@@ -2,7 +2,9 @@ package com.myinstagram;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootTest
 class MyInstagramApplicationTests {
 
